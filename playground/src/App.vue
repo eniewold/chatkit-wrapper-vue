36ff117cf97d351ca9999f978438e551e5db1af4
Edward Niewold <edward@niewold.com>
2025-11-27 08:02:50 +0200

    0.3.6 added sessionsUrl property

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
-import { ChatKit } from 'chatkit-wrapper-vue'
+    import { ChatKit } from 'chatkit-wrapper-vue'
 
-const workflowKey = 'demo-workflow-id'
+    const sessionsUrl = import.meta.env.VITE_CHATKIT_PROXY_URL;
+    const workflowKey = import.meta.env.VITE_OPENAI_WORKFLOW_KEY;
+
 </script>
 
 <template>
-  <main class="playground">
-    <header class="card">
-      <h1>ChatKit + Vue Playground</h1>
-      <p>
-        This playground links the local <code>chatkit-wrapper-vue</code> package via <code>file:..</code> so you can
-        experiment with the <code>&lt;ChatKit /&gt;</code> component while developing the library.
-      </p>
-    </header>
+    <main class="playground">
+        <header class="card">
+            <h1>ChatKit + Vue Playground</h1>
+            <p>
+                This playground links the local <code>chatkit-wrapper-vue</code> package via <code>file:..</code> so you can
+                experiment with the <code>&lt;ChatKit /&gt;</code> component while developing the library.
+            </p>
+        </header>
 
-    <section class="card">
-      <ChatKit
-        class="openai-chatkit"
-        :workflow-key="workflowKey"
-        :workflow-version="1"
-      />
-    </section>
-  </main>
+        <section class="card">
+            <ChatKit class="openai-chatkit"
+                     :sessionsUrl="sessionsUrl"
+                     :workflow-key="workflowKey"/>
+        </section>
+    </main>
 </template>
